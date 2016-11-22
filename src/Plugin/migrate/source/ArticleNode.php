15c92dfd031f57071d96eb99408e6e786249e1e9
Alvaro J. Hurtado <alvaroh@aspgems.com>
2016-11-22 19:40:34 +0100

    base fields migration for article nodes

--- src/Plugin/migrate/source/ArticleNode.php
+++ src/Plugin/migrate/source/ArticleNode.php
@@ -14,23 +14,51 @@ use Drupal\migrate\Row;
  */
 class ArticleNode extends SqlBase {
 
+  /**
+   * {@inheritdoc}
+   */
   public function query() {
-    $query = $this->select('node','a')
+    $query = $this->select('node', 'a')
+      ->condition('a.type', 'article')
       ->fields('a', [
         'nid',
         'title',
+        'vid',
+        'language',
+        'title',
+        'uid',
+        'status',
+        'created',
+        'changed',
+        'promote',
+        'sticky',
       ]);
     return $query;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function fields() {
-    $fields = [
-      'nid' => $this->t("Node NID"),
-      'title' => $this->t("Node Title"),
-    ];
+    $fields = $this->baseFields();
+    $fields['body/format'] = $this->t('Format of body');
+    $fields['body/value'] = $this->t('Full text of body');
+    $fields['body/summary'] = $this->t('Summary of body');
     return $fields;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    $nid = $row->getSourceProperty('nid');
+
+    return parent::prepareRow($row);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function getIds() {
     return [
       'nid' => [
@@ -39,5 +67,29 @@ class ArticleNode extends SqlBase {
       ],
     ];
   }
-}
 
+  /**
+   * Returns the node entity base fields to be migrated.
+   *
+   * @return array
+   *   Associative array having field name as key and description as value.
+   */
+  protected function baseFields() {
+    return [
+      'nid' => $this->t('Node ID'),
+      'vid' => $this->t('Version ID'),
+      'type' => $this->t('Type'),
+      'title' => $this->t('Title'),
+      'format' => $this->t('Format'),
+      'teaser' => $this->t('Teaser'),
+      'uid' => $this->t('Authored by (uid)'),
+      'created' => $this->t('Created timestamp'),
+      'changed' => $this->t('Modified timestamp'),
+      'status' => $this->t('Published'),
+      'promote' => $this->t('Promoted to front page'),
+      'sticky' => $this->t('Sticky at top of lists'),
+      'language' => $this->t('Language (es, en, ...)'),
+    ];
+  }
+
+}
