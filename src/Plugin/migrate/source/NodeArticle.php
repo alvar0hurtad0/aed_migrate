e68ca015564e4e6ee08c1b59646249aeafa4eda4
alvar0hurtad0 <alvaro.hurtado.villegas@gmail.com>
2016-11-27 22:52:28 +0000

    Migrate articles with source Node.

---
+++ src/Plugin/migrate/source/NodeArticle.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\aed_migration\Plugin\migrate\source;
+
+use Drupal\migrate\Row;
+use Drupal\node\Plugin\migrate\source\d7\Node;
+
+/**
+ * Drupal 7 node source from database.
+ *
+ * @MigrateSource(
+ *   id = "article_node"
+ * )
+ */
+class NodeArticle extends Node {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+
+    $nid = $row->getSourceProperty('nid');
+    $vid = $row->getSourceProperty('vid');
+
+    // Body.
+    $body = $this->getFieldValues('node', 'body', $nid, $vid);
+    $row->setSourceProperty('body_value', $body[0]['value']);
+    $row->setSourceProperty('body_format', 'full_html');
+    $row->setSourceProperty('body_summary', $body[0]['summary']);
+
+    return parent::prepareRow($row);
+
+  }
+
+}
