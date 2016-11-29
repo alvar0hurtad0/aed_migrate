9e0e11dd762df732ff76fcedf0703fd2509237ed
Alvaro J. Hurtado <alvaroh@aspgems.com>
2016-11-29 23:12:56 +0100

    video taxonomy terms added

--- src/Plugin/migrate/source/NodeVideos.php
+++ src/Plugin/migrate/source/NodeVideos.php
@@ -29,6 +29,26 @@ class NodeVideos extends Node {
     $row->setSourceProperty('body_format', 'full_html');
     $row->setSourceProperty('body_summary', $body[0]['summary']);
 
+    // Taxonomy terms.
+    $taxonomy_terms = [
+      'field_videos_audiencia',
+      'field_videos_ano',
+      'field_videos_evento',
+      'field_videos_idioma',
+      'field_videos_nivel',
+      'field_video_ponente',
+      'field_videos_version',
+    ];
+
+    foreach ($taxonomy_terms as $term) {
+      $value = [];
+      $source_term = $this->getFieldValues('node', $term, $nid, $vid);
+      foreach ($source_term as $item) {
+        $value[] = $item['tid'];
+      }
+      $row->setSourceProperty($term, $value);
+    }
+    
     return parent::prepareRow($row);
 
   }
